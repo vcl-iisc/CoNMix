87bce8ce0761fa82763c5bcb137dac920ea7d04f
HiPatil <hipatil1998@gmail.com>
2022-03-04 16:21:39 +0530

    training script

--- scripts/source_train.sh
+++ scripts/source_train.sh
@@ -1,7 +1,7 @@
-python3 source_train.py --gpu_id '2,1,0' --dset office-home --net deit_s --max_epoch 50 --interval 50 --batch_size 256 --s 0 --wandb 1 --output src_train --trte full
-python3 source_train.py --gpu_id '2,1,0' --dset office-home --net deit_s --max_epoch 50 --interval 50 --batch_size 256 --s 1 --wandb 1 --output src_train --trte full
-python3 source_train.py --gpu_id '2,1,0' --dset office-home --net deit_s --max_epoch 50 --interval 50 --batch_size 256 --s 2 --wandb 1 --output src_train --trte full
-python3 source_train.py --gpu_id '2,1,0' --dset office-home --net deit_s --max_epoch 50 --interval 50 --batch_size 256 --s 3 --wandb 1 --output src_train --trte full
+python3 source_train.py --gpu_id '2,1,0' --dset office-home --net deit_s --max_epoch 100 --interval 100 --batch_size 256 --s 0 --wandb 1 --output src_train --trte full
+# python3 source_train.py --gpu_id '2,1,0' --dset office-home --net deit_s --max_epoch 50 --interval 50 --batch_size 256 --s 1 --wandb 1 --output src_train --trte full
+# python3 source_train.py --gpu_id '2,1,0' --dset office-home --net deit_s --max_epoch 50 --interval 50 --batch_size 256 --s 2 --wandb 1 --output src_train --trte full
+# python3 source_train.py --gpu_id '2,1,0' --dset office-home --net deit_s --max_epoch 50 --interval 50 --batch_size 256 --s 3 --wandb 1 --output src_train --trte full
 
 python3 STDA.py --gpu_id '2,1,0' --s 0 --t 1 2 3 --dset office-home --net deit_s --max_epoch 50 --interval 50 --batch_size 128 --input_src src_train --wandb 1
 
